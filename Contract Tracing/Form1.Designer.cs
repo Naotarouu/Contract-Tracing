28c45ec5893918edae4e80d83c0e444d72c7e2b8
Naotarouu <darrencorpuz957@gmail.com>
2021-06-02 18:09:46 +0800

    Added program that take inputs from user and convert it to txt file.

--- Contract Tracing/Form1.Designer.cs
+++ Contract Tracing/Form1.Designer.cs
@@ -29,38 +29,38 @@ namespace Contract_Tracing
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.first_name = new System.Windows.Forms.TextBox();
             this.last_name = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
+            this.ageLabel = new System.Windows.Forms.Label();
             this.age = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
             this.email = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
             this.address = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
+            this.contactLabel = new System.Windows.Forms.Label();
             this.contact_number = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.date = new System.Windows.Forms.TextBox();
             this.time = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.bodytempLabel = new System.Windows.Forms.Label();
             this.body_temp = new System.Windows.Forms.TextBox();
             this.enterButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // name
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(38, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Name";
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.name.Location = new System.Drawing.Point(38, 32);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(51, 20);
+            this.name.TabIndex = 0;
+            this.name.Text = "Name";
             // 
             // label2
             // 
@@ -94,15 +94,15 @@ namespace Contract_Tracing
             this.last_name.Size = new System.Drawing.Size(161, 23);
             this.last_name.TabIndex = 4;
             // 
-            // label4
+            // ageLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(38, 120);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 20);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Age";
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ageLabel.Location = new System.Drawing.Point(38, 120);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(37, 20);
+            this.ageLabel.TabIndex = 5;
+            this.ageLabel.Text = "Age";
             // 
             // age
             // 
@@ -111,15 +111,15 @@ namespace Contract_Tracing
             this.age.Size = new System.Drawing.Size(100, 23);
             this.age.TabIndex = 6;
             // 
-            // label5
+            // emailLabel
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(196, 120);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(47, 20);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "Email";
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.emailLabel.Location = new System.Drawing.Point(196, 120);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(47, 20);
+            this.emailLabel.TabIndex = 7;
+            this.emailLabel.Text = "Email";
             // 
             // email
             // 
@@ -128,15 +128,15 @@ namespace Contract_Tracing
             this.email.Size = new System.Drawing.Size(187, 23);
             this.email.TabIndex = 8;
             // 
-            // label6
+            // addressLabel
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(38, 182);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(66, 20);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "Address";
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.addressLabel.Location = new System.Drawing.Point(38, 182);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(66, 20);
+            this.addressLabel.TabIndex = 9;
+            this.addressLabel.Text = "Address";
             // 
             // address
             // 
@@ -145,15 +145,15 @@ namespace Contract_Tracing
             this.address.Size = new System.Drawing.Size(342, 23);
             this.address.TabIndex = 10;
             // 
-            // label7
+            // contactLabel
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label7.Location = new System.Drawing.Point(37, 247);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(125, 20);
-            this.label7.TabIndex = 11;
-            this.label7.Text = "Contact Number";
+            this.contactLabel.AutoSize = true;
+            this.contactLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.contactLabel.Location = new System.Drawing.Point(37, 247);
+            this.contactLabel.Name = "contactLabel";
+            this.contactLabel.Size = new System.Drawing.Size(125, 20);
+            this.contactLabel.TabIndex = 11;
+            this.contactLabel.Text = "Contact Number";
             // 
             // contact_number
             // 
@@ -186,33 +186,33 @@ namespace Contract_Tracing
             this.time.Size = new System.Drawing.Size(100, 23);
             this.time.TabIndex = 15;
             // 
-            // label9
+            // dateLabel
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(222, 296);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(31, 15);
-            this.label9.TabIndex = 16;
-            this.label9.Text = "Date";
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Location = new System.Drawing.Point(222, 296);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(31, 15);
+            this.dateLabel.TabIndex = 16;
+            this.dateLabel.Text = "Date";
             // 
-            // label10
+            // timeLabel
             // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(328, 296);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(33, 15);
-            this.label10.TabIndex = 17;
-            this.label10.Text = "Time";
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(328, 296);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(33, 15);
+            this.timeLabel.TabIndex = 17;
+            this.timeLabel.Text = "Time";
             // 
-            // label11
+            // bodytempLabel
             // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label11.Location = new System.Drawing.Point(38, 318);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(139, 20);
-            this.label11.TabIndex = 18;
-            this.label11.Text = "Body Temperature";
+            this.bodytempLabel.AutoSize = true;
+            this.bodytempLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bodytempLabel.Location = new System.Drawing.Point(38, 318);
+            this.bodytempLabel.Name = "bodytempLabel";
+            this.bodytempLabel.Size = new System.Drawing.Size(139, 20);
+            this.bodytempLabel.TabIndex = 18;
+            this.bodytempLabel.Text = "Body Temperature";
             // 
             // body_temp
             // 
@@ -230,6 +230,7 @@ namespace Contract_Tracing
             this.enterButton.TabIndex = 20;
             this.enterButton.Text = "Enter";
             this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
             // 
             // Form1
             // 
@@ -239,25 +240,25 @@ namespace Contract_Tracing
             this.ClientSize = new System.Drawing.Size(467, 437);
             this.Controls.Add(this.enterButton);
             this.Controls.Add(this.body_temp);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
+            this.Controls.Add(this.bodytempLabel);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.dateLabel);
             this.Controls.Add(this.time);
             this.Controls.Add(this.date);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.contact_number);
-            this.Controls.Add(this.label7);
+            this.Controls.Add(this.contactLabel);
             this.Controls.Add(this.address);
-            this.Controls.Add(this.label6);
+            this.Controls.Add(this.addressLabel);
             this.Controls.Add(this.email);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.emailLabel);
             this.Controls.Add(this.age);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ageLabel);
             this.Controls.Add(this.last_name);
             this.Controls.Add(this.first_name);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.name);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Contract Tracing";
@@ -268,28 +269,29 @@ namespace Contract_Tracing
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label name;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox first_name;
         private System.Windows.Forms.TextBox last_name;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label ageLabel;
         private System.Windows.Forms.TextBox age;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label emailLabel;
         private System.Windows.Forms.TextBox email;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label addressLabel;
         private System.Windows.Forms.TextBox address;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label contactLabel;
         private System.Windows.Forms.TextBox contact_number;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox date;
         private System.Windows.Forms.TextBox time;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label bodytempLabel;
         private System.Windows.Forms.TextBox body_temp;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Label dateLabel;
     }
 }
 
--- Contract Tracing/Form1.cs
+++ Contract Tracing/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,32 @@ namespace Contract_Tracing
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            StreamWriter output = new StreamWriter(Application.StartupPath + "\\forms\\" + "output.txt");
+            output.WriteLine(name.Text + ":" + " " + first_name.Text + last_name.Text);
+            output.WriteLine();
+            output.WriteLine(ageLabel.Text + ":" + " " + age.Text);
+            output.WriteLine();
+            output.WriteLine(emailLabel.Text + ":" + " " + email.Text);
+            output.WriteLine();
+            output.WriteLine(addressLabel.Text + ":" + " " + address.Text);
+            output.WriteLine();
+            output.WriteLine(contactLabel.Text + ":" + " " + contact_number.Text);
+            output.WriteLine();
+            output.WriteLine(dateLabel.Text + ":" + " " + date.Text);
+            output.WriteLine();
+            output.WriteLine(timeLabel.Text + ":" + " " + time.Text);
+            output.WriteLine();
+            output.WriteLine(bodytempLabel.Text + ":" + " " + body_temp.Text);
+
+            output.Close();
         }
     }
 }
