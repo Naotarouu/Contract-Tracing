ae43e7bfd85db788c816a6e5315f120d7706b006
Naotarouu <darrencorpuz957@gmail.com>
2021-06-03 20:41:43 +0800

    Added label in form 2.

--- Contract Tracing/Form2.Designer.cs
+++ Contract Tracing/Form2.Designer.cs
@@ -30,35 +30,48 @@ namespace Contract_Tracing
         private void InitializeComponent()
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
             this.richTextBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.richTextBox1.Location = new System.Drawing.Point(40, 12);
+            this.richTextBox1.Location = new System.Drawing.Point(41, 31);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(313, 338);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Press any button to continue.";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.ClientSize = new System.Drawing.Size(393, 369);
+            this.ClientSize = new System.Drawing.Size(393, 392);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox1);
             this.MaximizeBox = false;
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Contract Tracing";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
